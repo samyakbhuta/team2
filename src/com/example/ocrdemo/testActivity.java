8560a2ca6f6bfe126cd35eab1c93d49fc3433d9d
harshkothari410 <harshkothari410@gmail.com>
2014-01-05 16:53:50 +0530

    testing card

--- src/com/example/ocrdemo/testActivity.java
+++ src/com/example/ocrdemo/testActivity.java
@@ -60,46 +60,46 @@ private void processPictureWhenReady(final String picturePath) throws IOExceptio
     if (pictureFile.exists()) {
         // The picture is ready; process it.
     	//Log.d("Picture Taken!",picturePath);
-    	Log.d("Start", "OCR Started!");
-    	boolean _taken = true;
-    	String _path = picturePath;
-    	BitmapFactory.Options options = new BitmapFactory.Options();
-        options.inSampleSize = 4;
-        	
-        Bitmap bitmap = BitmapFactory.decodeFile( _path, options );
-        ExifInterface exif = new ExifInterface(_path);
-    	int exifOrientation = exif.getAttributeInt(
-    	        ExifInterface.TAG_ORIENTATION,
-    	        ExifInterface.ORIENTATION_NORMAL);
-
-    	int rotate = 0;
-
-    	switch (exifOrientation) {
-    	case ExifInterface.ORIENTATION_ROTATE_90:
-    	    rotate = 90;
-    	    break;
-    	case ExifInterface.ORIENTATION_ROTATE_180:
-    	    rotate = 180;
-    	    break;
-    	case ExifInterface.ORIENTATION_ROTATE_270:
-    	    rotate = 270;
-    	    break;
-    	}
-
-    	if (rotate != 0) {
-    	    int w = bitmap.getWidth();
-    	    int h = bitmap.getHeight();
-
-    	    // Setting pre rotate
-    	    Matrix mtx = new Matrix();
-    	    mtx.preRotate(rotate);
-
-    	    // Rotating Bitmap & convert to ARGB_8888, required by tess
-    	    bitmap = Bitmap.createBitmap(bitmap, 0, 0, w, h, mtx, false);
-    	}
-    	bitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true);
-    	
-    	Log.d("Start", "OCR Ended");
+//    	Log.d("Start", "OCR Started!");
+//    	boolean _taken = true;
+//    	String _path = picturePath;
+//    	BitmapFactory.Options options = new BitmapFactory.Options();
+//        options.inSampleSize = 4;
+//        	
+//        Bitmap bitmap = BitmapFactory.decodeFile( _path, options );
+//        ExifInterface exif = new ExifInterface(_path);
+//    	int exifOrientation = exif.getAttributeInt(
+//    	        ExifInterface.TAG_ORIENTATION,
+//    	        ExifInterface.ORIENTATION_NORMAL);
+//
+//    	int rotate = 0;
+//
+//    	switch (exifOrientation) {
+//    	case ExifInterface.ORIENTATION_ROTATE_90:
+//    	    rotate = 90;
+//    	    break;
+//    	case ExifInterface.ORIENTATION_ROTATE_180:
+//    	    rotate = 180;
+//    	    break;
+//    	case ExifInterface.ORIENTATION_ROTATE_270:
+//    	    rotate = 270;
+//    	    break;
+//    	}
+//
+//    	if (rotate != 0) {
+//    	    int w = bitmap.getWidth();
+//    	    int h = bitmap.getHeight();
+//
+//    	    // Setting pre rotate
+//    	    Matrix mtx = new Matrix();
+//    	    mtx.preRotate(rotate);
+//
+//    	    // Rotating Bitmap & convert to ARGB_8888, required by tess
+//    	    bitmap = Bitmap.createBitmap(bitmap, 0, 0, w, h, mtx, false);
+//    	}
+//    	bitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true);
+//    	
+//    	Log.d("Start", "OCR Ended");
     	
     	//TessBaseAPI baseApi = new TessBaseAPI();
     	// DATA_PATH = Path to the storage
